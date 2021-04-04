3cd2a2737617bbee5515d0c1cbfd31ddb343c210
Ömer Acar <eomeracar@gmail.com>
2021-04-04 17:47:16 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Test");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
