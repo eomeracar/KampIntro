c776eac19f78a9f73853f037179dab520115f92a
Ömer Acar <eomeracar@gmail.com>
2021-04-05 02:48:37 +0300

    --Unnecessary codes are deleted.

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -7,47 +7,18 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            //MyList<string> names = new MyList<string>();
-            //names.Add("Test");
-            //names.Add("Test2");
-            //names.Add("Test3");
-            //List<string> names2 = new List<string>();
-
-            //Console.WriteLine(names);
-            //Console.WriteLine(names2.Count);
-
-
-            //foreach (var item in names.Items)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            //foreach (var item in names2)
-            //{
-
-            //}
-
-
-
-            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
-            
-            //Console.WriteLine(keyValuePairs.Keys);
-            //keyValuePairs.Add("01", "Adana");
-            //Console.WriteLine(keyValuePairs.Count);
-
-
             DynamicDictionary<int, string> myDictionary = new DynamicDictionary<int, string>();
             myDictionary.Add(37, "Kastamonu");
+            Console.WriteLine(myDictionary.Count);
+
             myDictionary.Add(37, "Kastamonu");
+
+            Console.WriteLine(myDictionary.Count); 
             foreach (var kvp in myDictionary.MyKeyandValuePairs)
             {
                 Console.WriteLine(kvp.Key  + "-->" + kvp.Value);
             }
 
-            //int[,] vs = new int[0,0];
-            //Console.WriteLine(vs.Length);
-            //vs[0,0]= 1;
-            //Console.WriteLine(vs[0,0]);
         }
     }
 }
